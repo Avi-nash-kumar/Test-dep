53d29dcd66c761c0160a25df9af7d4bcc2cb6476
Avinash Kumar <avipatel9196@gmail.com>
2023-06-01 11:06:28 +0530

    Initial commit

---
+++ src/main/java/com/test/controller/PersonController.java
@@ -0,0 +1,20 @@
+package com.test.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class PersonController {
+	
+
+	@RequestMapping("/getname")
+	public String getName() {
+		return "avinash";
+	}
+
+	
+	
+	
+
+}
